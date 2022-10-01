f420a39772cfc4195ee9065e227c46ce02a2f534
Nguyen Duc Thinh <ndthinh2@cmcglobal.vn>
2022-10-01 14:38:20 +0700

    nopbai

---
+++ company/ss3_mang/bai_tap/MangDaChieu.java
@@ -0,0 +1,18 @@
+package company.ss3_mang.bai_tap;
+
+import java.util.Scanner;
+
+public class MangDaChieu {
+    public static void main(String[] args) {
+        int[][] myArray = new int[5][5];
+        int input;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter " + myArray.length + " rows and " +
+                myArray[0].length + " columns: ");
+        for (int i = 0; i <myArray.length ; i++) {
+            for (int j = 0; j <myArray[i].length ; j++) {
+
+            }
+        }
+    }
+}
--- company/ss3_mang/bai_tap/MaxValue.java
+++ company/ss3_mang/bai_tap/MaxValue.java
@@ -1,19 +1,34 @@
 package company.ss3_mang.bai_tap;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MaxValue {
     public static void main(String[] args) {
-        int[] list1 = {1,2,3,1,5,8,29};
-        int[] list2 = {8,20,15,19,27,18};
-        int max = 0;
-        for (int i = 0; i <list1.length ; i++) {
-            for (int j = 0; j <list2.length ; j++) {
-                if (list1[i] > list2[j]) {
-                    max = list1[i];
-                } else {
-                    max = list2[j];
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số hàng: ");
+        int row = sc.nextInt();
+        System.out.print("Nhập số cột:");
+        int col = sc.nextInt();
+
+        int[][] arr = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.println("Nhập phần tử thứ " + row + "-" + col + ": ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Mảng đã cho: " + Arrays.toString(arr));
+
+        int max = arr[0][0];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
                 }
             }
-            System.out.println(max);
         }
+
+        System.out.println("Phần tử lớn nhất của mảng là: " + max);
     }
 }
